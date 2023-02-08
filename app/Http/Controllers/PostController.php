e5f0331e397b7f91d2a4c61dc85a5ca1e9facf82
josepserra20 <jserravives@cifpfbmoll.eu>
2023-02-08 16:36:05 +0100

    form

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('form')->with([
+            'title' => old('title'),
+            'description' => old('description'),
+            'content' => old('content'),
+            'caducable' => old('caducable'),
+            'comentable' => old('comentable'),
+            'acceso' => old('acceso')
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request) :JsonResponse
+    {
+        // Lógica para almacenar la publicación
+        $request->validate([
+            'title' => 'required|min:10',
+        ]);
+
+        $post = new Post();
+        $post->title = $request->get('title');
+        $post->description = $request->get('description');
+        $post->content = $request->get('content');
+        $post->caducable = $request->get('caducable') ?? false;
+        $post->acceso = $request->get('acceso') ?? false;
+        $post->comentable = false;
+
+        $user = Auth::user();
+        $post->user_id = $user->id;
+        $post->save();
+
+        return Response::json($request->all());
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        if (Gate::denies('update-post', $id)) {
+            abort(403, 'No tienes permiso para actualizar esta publicación.');
+        }
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/form.blade.php
@@ -0,0 +1,50 @@
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+<form method="POST" action="{{ route('form.store') }}">
+    @csrf
+    <div class="form-group">
+        <label for="title">{{ __('form.title') }}</label>
+        <input type="text" name="title" class="form-control" value="{{ $title }}" required>
+    @if ($errors->has('title'))
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $errors->first('title') }}</strong>
+        </span>
+    @endif
+    </div>
+    <div class="form-group">
+        <label for="description">{{ __('form.description') }}</label>
+        <textarea name="description" class="form-control" required>{{ $description }}</textarea>
+    </div>
+    <div class="form-group">
+        <label for="content">{{ __('form.content') }}</label>
+        <textarea name="content" class="form-control" required>{{ $content }}</textarea>
+    </div>
+    <div class="form-group">
+        <label> {{ __('form.caducable') }}</label>
+        <div class="form-check">
+        <input type="hidden" name="caducable" value="0">
+            <input class="form-check-input" type="checkbox" name="caducable" value="1" {{ $caducable ? 'checked' : '' }}>
+            <label class="form-check-label">Marca esta casilla si deseas que la publicación caduque</label>
+        </div>
+    </div>
+    <div class="form-group">
+        <label> {{ trans('form.comentable') }}</label>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" name="comentable" value="1" {{ $comentable ? 'checked' : '' }}>
+            <label class="form-check-label">Marca esta casilla si deseas que los usuarios puedan comentar en la publicación</label>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="acceso">{{ trans('form.acceso') }}</label>
+        <select name="acceso" class="form-control">
+            <option value="privado" {{ $acceso == 'privado' ? 'selected' : ''}} >{{ trans('form.privado')}}</option>
+                <option value="privado" {{ $acceso == 'publico' ? 'selected' : ''}} > {{trans('form.publico') }} </option>
+    <input type="submit" value="Submit">
