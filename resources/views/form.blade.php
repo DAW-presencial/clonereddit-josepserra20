e3b700c25a9b3626ef0e6dd02d1dc3b00570cb5c
josepserra20 <jserravives@cifpfbmoll.eu>
2023-02-11 12:38:28 +0100

    style: css
    css form

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,36 +1,43 @@
-@if ($errors->any())
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    </head>
+    <body class="container">
+    @if ($errors->any())
     <div class="alert alert-danger">
         <ul>
             @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
+            <li>{{ $error }}</li>
             @endforeach
         </ul>
     </div>
-@endif
-
-<form method="POST" action="{{ route('form.store') }}">
-    @csrf
-    <div class="form-group">
-        <label for="title">{{ __('form.title') }}</label>
-        <input type="text" name="title" class="form-control" value="{{ $title }}" required>
-    @if ($errors->has('title'))
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $errors->first('title') }}</strong>
-        </span>
     @endif
-    </div>
-    <div class="form-group">
-        <label for="description">{{ __('form.description') }}</label>
-        <textarea name="description" class="form-control" required>{{ $description }}</textarea>
-    </div>
-    <div class="form-group">
-        <label for="content">{{ __('form.content') }}</label>
+    <form method="POST" action="{{ route('form.store') }}">
+        @csrf
+        <div class="form-group">
+            <label for="title">{{ __('form.title') }}</label>
+            <input type="text" name="title" class="form-control" value="{{ $title }}" required>
+            @if ($errors->has('title'))
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $errors->first('title') }}</strong>
+            </span>
+            @endif
+        </div>
+        <div class="form-group">
+            <label for="description">{{ __('form.description') }}</label>
+            <textarea name="description" class="form-control" required>{{ $description }}</textarea>
+        </div>
+        <div class="form-group">
+            <label for="content">{{ __('form.content') }}</label>
         <textarea name="content" class="form-control" required>{{ $content }}</textarea>
     </div>
     <div class="form-group">
         <label> {{ __('form.caducable') }}</label>
         <div class="form-check">
-        <input type="hidden" name="caducable" value="0">
+            <input type="hidden" name="caducable" value="0">
             <input class="form-check-input" type="checkbox" name="caducable" value="1" {{ $caducable ? 'checked' : '' }}>
             <label class="form-check-label">Marca esta casilla si deseas que la publicación caduque</label>
         </div>
@@ -46,5 +53,7 @@
         <label for="acceso">{{ trans('form.acceso') }}</label>
         <select name="acceso" class="form-control">
             <option value="privado" {{ $acceso == 'privado' ? 'selected' : ''}} >{{ trans('form.privado')}}</option>
-                <option value="privado" {{ $acceso == 'publico' ? 'selected' : ''}} > {{trans('form.publico') }} </option>
-    <input type="submit" value="Submit">
+            <option value="privado" {{ $acceso == 'publico' ? 'selected' : ''}} > {{trans('form.publico') }} </option>
+            <input type="submit" value="Submit">
+    </body>
+</html>
